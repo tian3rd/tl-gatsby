832f336eed547f47fc2dbe46ea85df86497c6bbb
tian <tianwu@outlook.com>
2021-08-29 22:53:57 +1000

    add mockup img on the right

--- src/components/animations/MockupAnimation.tsx
+++ src/components/animations/MockupAnimation.tsx
@@ -3,20 +3,41 @@ import styled from "styled-components"
 
 function MockupAnimation() {
   return (
-    <Wrappper>
+    <Wrapper>
       {/* mockups for future real pics replacement */}
-      <div className="mockup1"></div>
-    </Wrappper>
+      {/* <div className="mockup1"></div> */}
+      <div className="mockup1" />
+    </Wrapper>
   )
 }
 
 export default MockupAnimation
 
-const Wrappper = styled.div`
+const Wrapper = styled.div`
   position: relative;
-  background: url("/images/animations/computer-herosection.svg");
-  .mockup1 {
+
+  @media (max-width: 768px) {
+    transform: scale(0.56);
+    transform-origin: top left;
   }
+  @media (max-width: 450px) {
+    transform: scale(0.5);
+    transform-origin: top left;
+  }
+
+  .mockup1 {
+    /* mockup1 copied from Figma */
+    position: absolute;
+    width: 661px;
+    height: 496px;
+    left: 0px;
+    top: 0px;
+    background: url("/images/animations/computer-herosection.svg");
+    backdrop-filter: blur(10px);
+    /* Note: backdrop-filter has minimal browser support */
+    border-radius: 16px;
+  }
+
   & {
     transition: 1.5s cubic-bezier(0.075, 0.82, 0.165, 1);
   }
