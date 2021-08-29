13f8ccf716efaa4dda5643f6d9feeb5bcfe88259
tian <tianwu@outlook.com>
2021-08-29 10:43:44 +1000

    use typescript in gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
+    `gatsby-plugin-typescript`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
