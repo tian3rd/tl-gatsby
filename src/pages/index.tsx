396cf8058586e134212d5a9ee8ef99a994f9da61
tian <tianwu@outlook.com>
2021-08-29 22:56:50 +1000

    remove extra btns

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,10 @@
 import * as React from "react"
 import Layout from "../components/layout/layout"
-import ButtonComponent from "../components/buttons/ButtonComponent"
 import HeroSection from "../components/sections/HeroSection"
 const IndexPage = () => (
   <>
     <Layout>
       <HeroSection />
-      <ButtonComponent />
     </Layout>
   </>
 )
