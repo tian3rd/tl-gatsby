c7a6a4eeeae269ffaa3c53bf43e30bf8957b5868
tian <tianwu@outlook.com>
2021-08-29 22:57:39 +1000

    add waves to hero section; apply gradient to title text

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -4,10 +4,12 @@ import RequestButton from "../buttons/RequestButton"
 import { H1, MediumText } from "../styles/TextStyles"
 import { themes } from "../styles/ColorStyles"
 import MockupAnimation from "../animations/MockupAnimation"
+import WaveBackgound from "../backgrounds/WaveBackgound"
 
 const HeroSection = () => {
   return (
     <Wrapper>
+      <WaveBackgound />
       <ContentWrapper>
         <TextWrapper>
           <Title>
@@ -34,23 +36,25 @@ const HeroSection = () => {
 export default HeroSection
 
 const Wrapper = styled.div`
-  background: linear-gradient(
-    115.82deg,
-    rgba(0, 35, 123, 0.7) 0%,
-    rgba(235, 119, 118, 0.9) 93.65%
-  );
   /* prevent future mockups of overflowing outside*/
   overflow: hidden;
 `
 
 const ContentWrapper = styled.div`
   /* don't use width */
-  max-width: 1234px;
+  max-width: 1300px;
   /* normal center trick */
   margin: 0 auto;
   padding: 200px 30px;
   display: grid;
   grid-template-columns: 540px auto;
+
+  /* for mobile devices of < 450px wide */
+  @media (max-width: 768px) {
+    grid-template-columns: auto;
+    gap: 60px;
+    padding: 150px 20px 250px;
+  }
 `
 
 const TextWrapper = styled.div`
@@ -61,6 +65,10 @@ const TextWrapper = styled.div`
 
 const Title = styled(H1)`
   color: ${themes.dark.text1};
+  background: linear-gradient(94.87deg, #be1e69 0%, #eb7776 53.11%);
+  background-clip: text;
+  --webkit-background-clip: text;
+  color: transparent;
 `
 
 const Description = styled(MediumText)``
